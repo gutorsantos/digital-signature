9a1e133ce70bfab26467b49fca73de65ae7b08f5
Gustavo Santos <gustavo.r.santos@outlook.com>
2021-10-26 15:35:50 -0300

    correcoes

--- miller_rabin.py
+++ miller_rabin.py
@@ -1,26 +1,26 @@
 import random
-die = random.SystemRandom() # A single dice.
+r = random.SystemRandom()
 
-def single_test(n, a):
-    exp = n - 1
-    while not exp & 1:
-        exp >>= 1
+def test(n, a):
+    e = n - 1
+    while not e & 1:
+        e >>= 1
         
-    if pow(a, exp, n) == 1:
+    if pow(a, e, n) == 1:
         return True
         
-    while exp < n - 1:
-        if pow(a, exp, n) == n - 1:
+    while e < n - 1:
+        if pow(a, e, n) == n - 1:
             return True
             
-        exp <<= 1
+        e <<= 1
         
     return False
     
 def miller_rabin(n, k=10):
     for i in range(k):
-        a = die.randrange(2, n - 1)
-        if not single_test(n, a):
+        a = r.randrange(2, n - 1)
+        if not test(n, a):
             return False
             
     return True
